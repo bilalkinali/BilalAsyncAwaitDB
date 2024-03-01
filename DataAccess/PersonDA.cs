90644bedf84125f7e841e0f170aa03abf2e49753
Bilal Kinali <biki59986@edu.ucl.dk>
2024-03-02 00:19:45 +0100

    Added SQL connection - test

--- DataAccess/PersonDA.cs
+++ DataAccess/PersonDA.cs
@@ -1,33 +1,21 @@
 ﻿using Models;
+using System.Configuration;
+using System.Data.SqlClient;
 
 
 namespace DataAccess
 {
     public class PersonDA
     {
-        List<Person> people;
+        string path;
 
         public PersonDA()
         {
-            people = new List<Person>()
-            {
-                new Person { Id = 1, FirstName = "Natalie", LastName = "Richards", Address = "25322 Rivera Stream", City = "New Nathan", PostalCode = 9253, Email = "tkelly@lawson.com", Phone = 49412066 },
-                new Person { Id = 2, FirstName = "Natasha", LastName = "Stewart", Address = "25573 Anne Motorway", City = "Thomasshire", PostalCode = 4239, Email = "fgentry@yahoo.com", Phone = 97135562 },
-                new Person { Id = 3, FirstName = "Rachel", LastName = "Brooks", Address = "1819 Gregory Street", City = "South Amberland", PostalCode = 6098, Email = "jordan02@stokes-webb.net", Phone = 92870117 },
-                new Person { Id = 4, FirstName = "Andrew", LastName = "Oconnor", Address = "21038 Michelle Trail", City = "East Gregoryville", PostalCode = 2559, Email = "ldaniels@moore.info", Phone = 67980075 },
-                new Person { Id = 5, FirstName = "April", LastName = "Mendoza", Address = "6498 Jennifer Keys Apt. 785", City = "Lake Caroline", PostalCode = 3358, Email = "lance80@yahoo.com", Phone = 87896283 },
-                new Person { Id = 6, FirstName = "Vanessa", LastName = "Nelson", Address = "237 Miller Inlet", City = "Port Erin", PostalCode = 7482, Email = "dgriffin@yahoo.com", Phone = 29589553 },
-                new Person { Id = 7, FirstName = "Deborah", LastName = "Frey", Address = "2432 Jennifer Crescent", City = "Raymondton", PostalCode = 3235, Email = "lwalker@yahoo.com", Phone = 93287558 },
-                new Person { Id = 8, FirstName = "Kimberly", LastName = "Ferrell", Address = "055 Matthew Course", City = "South Michael", PostalCode = 9656, Email = "hansenrobert@king.info", Phone = 98095925 },
-                new Person { Id = 9, FirstName = "Robert", LastName = "Tyler", Address = "25490 Barbara Extensions", City = "Lake Jasonport", PostalCode = 8784, Email = "david09@yahoo.com", Phone = 52334412 },
-                new Person { Id = 10, FirstName = "Tony", LastName = "Fletcher", Address = "38388 Sanchez River", City = "Lake Michaelside", PostalCode = 3632, Email = "westyvonne@hotmail.com", Phone = 61472543 }
-            };
+            path = ConfigurationManager.ConnectionStrings["default"].ToString();
         }
 
         public async Task<bool> CreateAsync(Person p)
         {
-            await Task.Delay(3000);
-
             int id = people.Max(x => x.Id) + 1;
 
             p.Id = id;
@@ -40,6 +28,35 @@ namespace DataAccess
         public async Task<List<Person>> GetAsync()
         {
             await Task.Delay(3000);
+
+            List<Person> people = new List<Person>();
+
+            string command = "SELECT * FROM PERSON";
+
+            using SqlConnection dbConn = new SqlConnection(path);
+
+            await dbConn.OpenAsync();
+
+            using SqlCommand sqlCommand = new SqlCommand(command, dbConn);
+
+            SqlDataReader reader = await sqlCommand.ExecuteReaderAsync();
+
+            while (await reader.ReadAsync())
+            {
+                Person p = new Person();
+                p.Id = (int)reader["ID"];
+                p.FirstName = (string)reader["FirstName"];
+                p.LastName = (string)reader["LastName"];
+                p.Address = (string)reader["Address"];
+                p.City = (string)reader["City"];
+                p.PostalCode = (int)reader["PostalCode"];
+                p.Email = (string)reader["Email"];
+                p.Phone = (int)reader["Phone"];
+                people.Add(p);
+            }
+
+            await dbConn.CloseAsync();
+
             return people;
         }
 
